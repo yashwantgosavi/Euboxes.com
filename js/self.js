a1271132eac1b609f2514b3330e2b7c13d95802c
yashwantgosavi <yashwantgosavi0@gmail.com>
2023-08-14 21:25:32 +0530

    first commit

---
+++ js/self.js
@@ -0,0 +1,9 @@
+//offer patch closing function
+function closeMark() {
+  var x = document.getElementById("close");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
